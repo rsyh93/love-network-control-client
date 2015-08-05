26680d1f9da19d2728ca07e3efa4dfb9c55ec7f9
Young Hoon Kim <rsyh93@gmail.com>
2015-08-05 17:09:21 -0400

    add more configurations

--- conf.lua
+++ conf.lua
@@ -1,7 +1,15 @@
 function love.conf(t)
   t.title = "Playstation client"
   t.version = "0.9.2"
+
+  t.window.icon = nil       -- default
   t.window.width = 600
   t.window.height = 372
+  t.window.resizable = true -- default
+
+  t.modules.audio = false
+  t.modules.math = false
   t.modules.physics = false
+
+  t.console = false         -- display console on windows
 end
